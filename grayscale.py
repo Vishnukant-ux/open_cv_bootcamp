970ccce144449928b152c3249cfd89277e508460
Vishnu kant prasad <vkant4252@gmail.com>
2026-06-17 13:54:42 +0530

    Create grayscale.py

---
+++ grayscale.py
@@ -0,0 +1,8 @@
+import cv2
+
+img = cv2.imread("checker_board.jpg", 0)
+
+print("Shape:", img.shape)
+print("Datatype:", img.dtype)
+
+img that I used=(https://commons.wikimedia.org/wiki/File:Checkerboard_pattern.svg)
