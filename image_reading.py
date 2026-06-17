02e437459628d8d5e953bcacb40e731f6cfe154d
Vishnu kant prasad <vkant4252@gmail.com>
2026-06-17 13:39:23 +0530

    Create image_reading.py

---
+++ image_reading.py
@@ -0,0 +1,9 @@
+import cv2
+
+img = cv2.imread("checker_board.jpg")
+
+print("Shape:", img.shape)
+print("Datatype:", img.dtype)
+
+img that I used=(https://commons.wikimedia.org/wiki/File:Checkerboard_pattern.svg)
+
